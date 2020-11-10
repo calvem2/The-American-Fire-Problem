31bd33bfc3fcd12d8fcdc54b92739359d6338e0c
shurygij <yoni.shurygin@gmail.com>
2020-11-09 20:35:50 -0800

    adding fires per 10 thousand acres to the existing firecount CSV, titled firesPerAcre.csv

--- data_workplace.py
+++ data_workplace.py
@@ -21,3 +21,18 @@ df = pd.read_csv('docs/over0.5AcreWithIDs.csv')
 df = df.loc[df["STATE"] != "Puerto Rico"]
 df.to_csv('docs/over0.5AcreWithIDs(2).csv', index=False)
 '''
+
+#setting up fires per acre
+df = pd.read_csv('docs/fireCountsOver0.5Acre.csv')
+df2 = pd.read_csv('acres.csv')
+df2 = df2[["State", "LandArea"]]
+firesPerAcre = []
+for i in range(len(df)):
+    fireCount = df.iloc[i]['FIRE_COUNT']
+    state = df.iloc[i]['STATE'] if df.iloc[i]['STATE'] != "District of Columbia" else "Washington DC"
+    acreage = df2.loc[df2['State'] == state]['LandArea'].values[0] * 600
+    firesPerAcre.append(fireCount / acreage * 10000)
+
+df['FIRES_PER_10K_ACRE'] = firesPerAcre
+df.to_csv("firesPerAcre.csv", index=False)
+print("DONE")
